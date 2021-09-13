892d5e74d9152a11d08a71073707c7347f6bb89e
goofyjnr <51934302+goofyjnr@users.noreply.github.com>
2021-09-14 10:35:15 +1200

    added the lost items models

--- Group/lostandfound/models.py
+++ Group/lostandfound/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class LostItem(models.Model):
+    typeofitem = models.CharField(max_length=2000)
+    description = models.CharField(max_length=2000)
+    datelost = models.DateField()
+    contactinfo = models.CharField(max_length=2000)
+    name = models.TextField(max_length=50)
+
+
+class FoundItem(models.Model):
+    pass
+
+class Person(models.Model):
+    pass
