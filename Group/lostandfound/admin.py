72730c833cdb76a4f0befedc6235da6f62984001
goofyjnr <51934302+goofyjnr@users.noreply.github.com>
2021-09-27 12:41:00 +1300

    added views that show lost items
    finished up to part 6 in sirs videos

--- Group/lostandfound/admin.py
+++ Group/lostandfound/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import LostItem
 
 # Register your models here.
+admin.site.register(LostItem)
--- Group/lostandfound/views.py
+++ Group/lostandfound/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import LostItem
 
 # Create your views here.
 
 def index(request):
-    #code here
 
-    return HttpResponse('you made it to index')
+    all_lost = LostItem.objects.all()
+
+    context = {"all_lost" : all_lost}
+
+    return render(request, 'lostandfound/index.html',context)
