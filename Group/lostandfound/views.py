a05b8972657beb42aabb6a1850e0e788fcfe452a
goofyjnr <51934302+goofyjnr@users.noreply.github.com>
2021-09-28 10:24:24 +1300

    added filters
    part 7 of sirs videos

--- Group/lostandfound/views.py
+++ Group/lostandfound/views.py
@@ -1,13 +1,19 @@
+from django.db.models.fields import DateField
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import LostItem
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
 
-    all_lost = LostItem.objects.all()
+    all_lost = LostItem.objects.all().order_by("datelost")
 
-    context = {"all_lost" : all_lost}
+    lost_phones = all_lost.filter(typeofitem__contains = "phone")
+
+    lost_today = all_lost.filter(datelost = datetime.now())
+
+    context = {"all_lost" : all_lost, "lost_phones": lost_phones, "lost_today":lost_today}
 
     return render(request, 'lostandfound/index.html',context)
